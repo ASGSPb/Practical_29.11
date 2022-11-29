bdd8853d28ab0d30ede542464e5130e400afeb2b
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-30 01:23:10 +0300

    practical task этап 1

---
+++ Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+Console.Write("Задайте n - колличество строк. n =  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте m - колличество столбцов. m =  ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+double [,] array2D = new double [n, m];
+
+for (int i = 0; i < array2D.GetLength(0); i++)
+{
+    System.Console.WriteLine();
+    
+    for (int j = 0; j < array2D.GetLength(1); j++)
+    {
+        array2D[i, j] = Math.Round((new Random().Next(-10, 10) + new Random().NextDouble()), 2);
+
+        Console.Write(String.Format("{0,6}", array2D[i, j]) + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
---
+++ Task02/Program.cs
@@ -0,0 +1,63 @@
+﻿// Напишите программу, 
+// которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+
+Console.Clear();
+Console.WriteLine("Задайте позицию интересующего Вас элемента массива N [x, y]   ");
+
+Console.Write("x =  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y =  ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int n = new Random().Next(2, 7 );
+Console.WriteLine($"n = {n}");
+int m = new Random().Next(2, 7 );
+Console.WriteLine($"m= {m}");
+
+
+int[,] array2D = FillingArray2D(n, m);
+
+if ( x < n && y < m )
+{
+    Console.WriteLine($"На позиции элемента N [{x}, {y}] находится элемент {array2D[x, y]} ");
+}
+else 
+{
+    Console.WriteLine($"Массив не содержит элемент с указанной позицией N [{x}, {y}]");
+    Console.WriteLine();
+}
+
+
+int[,] FillingArray2D(int n, int m)
+{
+    int [,] array2D = new int [n, m];
+
+    for (int j = 0; j < array2D.GetLength(1); j++)
+    {
+        System.Console.WriteLine();
+    
+        for (int i = 0; i < array2D.GetLength(0); i++)
+        {
+            array2D[i, j] = new Random().Next(0, 10);
+
+            Console.Write(String.Format("{0,6}", array2D[i, j]) + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return array2D;
+}
