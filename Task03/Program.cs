d72d3fe244c06ec6017d5a4605ff36d718a7c167
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-30 11:51:06 +0300

    practical task этап 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -6,47 +6,51 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
-
 Console.Clear();
-Console.WriteLine("В 2D массиве  ");
+Console.Write("В 2D массиве  ");
 
 int n = new Random().Next(2, 5);
 int m = new Random().Next(2, 5);
 
-int[,] array2D = FillingArray2D(n, m);
+Console.WriteLine($"колво столбцов {n} колво строк {m}");
+// int n = 2;
+// int m = 3;
 
-for (int i = 0; i < n; i++)
-{
-    double averageValue = 0;
-    double count = 1;
-    System.Console.WriteLine();
+int [,] array2D = new int[n, m];
 
-    for (int j = 0; j < m; j++)
-    {
-        averageValue = (array2D[i, j] + averageValue) / count;
-        count++;
-    }
-    Console.WriteLine($"Среднее арифметическое столбца {i} равно {Math.Round(averageValue, 2)} ");
-}
-Console.WriteLine();
-
-int[,] FillingArray2D(int n, int m)
-{
-    int[,] array2D = new int[n, m];
-
-    for (int j = 0; j < array2D.GetLength(1); j++)
+for (int j = 0; j < m; j++)
     {
         System.Console.WriteLine();
 
-        for (int i = 0; i < array2D.GetLength(0); i++)
+        for (int i = 0; i < n; i++)
         {
             array2D[i, j] = new Random().Next(0, 10);
 
             Console.Write(String.Format("{0,6}", array2D[i, j]) + " ");
+            // Thread.Sleep(1000);
         }
         Console.WriteLine();
     }
-    Console.WriteLine();
-    return array2D;
-}
+
+
+for (int i = 0; i < n; i++)
+{
+    double averageValue = 0;
+    double count = 0;
+    System.Console.WriteLine();
+
+    for (int j = 0; j < m; j++)
+    {
+        // System.Console.WriteLine($"count на входе = {count}");
+        averageValue = array2D[i, j] + averageValue;
+        count = j + 1;
+        // System.Console.WriteLine($"элемент  array2D[{i}, {j}] = {array2D[i, j]}");
+        // System.Console.WriteLine($" сумма = {averageValue}");
+    }
+    // Console.Write($" среднеариф столбца {i} : {averageValue} / {count} = {averageValue / count}");
+    Console.WriteLine($"Среднее арифметическое столбца {i} равно {Math.Round(averageValue / count, 2)} ");
+    Thread.Sleep(1000);
+    }
+Console.WriteLine();
+
+
